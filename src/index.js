0ae1f01cb198488b1a3a539abf07dae744d9e78a
unknown <gitstream007>
2020-05-25 15:13:33 +0200

    map & inside return deleted

--- src/index.js
+++ src/index.js
@@ -7,31 +7,37 @@ class App extends React.Component {
 
   state = {
     consoles: [
-      {id: 1, nom: "Master System"},
-      {id: 2, nom: "Megadrive"},
-      {id: 3, nom: "Snes"},
-      {id: 4, nom: "Arcade"}
+      { id: 1, nom: "Master System" },
+      { id: 2, nom: "Megadrive" },
+      { id: 3, nom: "Snes" },
+      { id: 4, nom: "Arcade" }
     ]
   }
 
   render() {
-    const title= "Liste de consoles";
+    const title = "Liste de consoles";
     const element = <li>Test variable</li>
+    /* 
+     const elements = [
+         <li>first variable</li>,
+         <li>second variable</li>,
+         <li>third variable</li>
+     ] 
+     */
+    const elements = this.state.consoles.map((console) =>
+      <li>{console.nom}<button>X</button></li>
+    )
     return (
-    <div>
-      <h1>{title}</h1>
-    <ul> 
-      {element}
-      <li> Master System <button>X</button></li>
-      <li> Megadrive <button>X</button></li>
-      <li> Snes <button>X</button></li>
-      <li> Arcade <button>X</button></li>
-    </ul>
-    <form>
-      <input type="text" placeholder="Ajouter une console" />
-      <button>Confirmer</button>
-      </form>
-    </div>
+      <div>
+        <h1>{title}</h1>
+        <ul>
+          {elements}
+        </ul>
+        <form>
+          <input type="text" placeholder="Ajouter une console" />
+          <button>Confirmer</button>
+        </form>
+      </div>
     );
   }
 }
